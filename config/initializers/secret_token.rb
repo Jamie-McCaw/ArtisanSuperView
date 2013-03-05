798d3efe9eef6d40ac18bd327d78574fdad17295
Jamie-McCaw <psiral@yahoo.com>
2013-03-05 10:20:14 -0500

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArtisanSuperView::Application.config.secret_token = '0b26424d8868ff73a4f23fb5e7e370a6df72527fd9d57239c0b48207f4a7e97173e72d551ba3eb0a25615868c8bd49ffa596191abd9afe2e4262728c748684a4'
