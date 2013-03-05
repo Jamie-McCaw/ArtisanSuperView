ebbd8c69893eb8d7bb1d797215fbaf11cdddc71b
Jamie-McCaw <psiral@yahoo.com>
2013-03-05 15:15:08 -0500

    List View Table created

--- features/open_page.feature
+++ features/open_page.feature
@@ -1,13 +1,7 @@
 Feature: User opens list view
 
 
-	Scenario: Page opened
+	Scenario: Home Page opened.
 		Given a user visits list view page
 		When the user clicks on the List View link
-		Then the user should see the list of Project names
-		And the user should see the iteration number
-		And the user should see the members names
-		And the user should see the total points in the Ready column
-		And the user should see the total points in the Working column
-		And the user should see the total points in the completed column
-		And the user should see if the project is completed or not
+		Then the user should see a table of information
--- features/step_definitions/list_view_steps.rb
+++ features/step_definitions/list_view_steps.rb
@@ -8,30 +8,6 @@ When(/^the user clicks on the List View link$/) do
 	click_link "List View"
 end
 
-Then(/^the user should see the list of Project names$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then(/^the user should see the iteration number$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then(/^the user should see the members names$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then(/^the user should see the total points in the Ready column$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then(/^the user should see the total points in the Working column$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then(/^the user should see the total points in the completed column$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then(/^the user should see if the project is completed or not$/) do
-  pending # express the regexp above with the code you wish you had
+Then(/^the user should see a table of information/) do
+  page.should have_selector('.table')
 end
