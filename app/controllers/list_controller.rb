1bc64703dd2d28649a14824adfee3d09fceffe3d
Jamie-McCaw <psiral@yahoo.com>
2013-03-07 16:24:44 -0500

    Refactored List Controller

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,44 +1,6 @@
 class ListController < ApplicationController
 
-  def view
-  	api_keys = Keys.new.get_keys
-  	projects = Array.new
-  	api_keys.each do |api_key|
-			project_name = Project.new(api_key).get_project_name
-			project_iterations = Iterations.new(api_key).get_all_project_iterations
-			projects.push(build_row_hash(project_iterations, project_name, api_key))
-		end
-		@iterations = projects.flatten
-  end
-
-  def build_row_hash(iterations, project_name, api_key)
-  	table = Array.new
-		iterations.each do |iteration|
-			row = Hash.new
-			row.store(:project_name, project_name)
-			row.store(:iteration_number, iteration[:number])
-			row.store(:members, get_names(api_key, iteration[:number]))
-			row.store(:billed_points, iteration[:total_billed_points])
-			if iteration[:committed_points_at_completion] != nil
-				row.store(:total_bug_points, iteration[:committed_points_at_completion].to_f - iteration[:total_billed_points])
-			else
-				row.store(:total_bug_points, 0)
-			end
-			if iteration[:committed_points_at_completion] != nil
-				row.store(:completed_points, iteration[:committed_points_at_completion])
-			else
-				row.store(:completed_points, 0)
-			end
-			row.store(:iteration_completed?, iteration[:complete])
-			table.push(row)
-		end
-		return table 	
-  end
-
-  def get_names(api_key, iteration_number)
-  	stories = Stories.new(api_key, iteration_number)
-  	members = stories.get_craftsman_names 
-  end
+	def view
+		@iterations = Tables.new.create_project_table
+	end
 end
-
-
